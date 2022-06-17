a6ea2a5753fe2329b52675b574213ed8748193d4
Mercy <68203696+MercyVasquez@users.noreply.github.com>
2022-06-17 18:38:39 -0400

    cambios titulos

--- public/admin_products.php
+++ public/admin_products.php
@@ -86,7 +86,7 @@ if(isset($_POST['update_product'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>products</title>
+   <title>SHIMEJIRD</title>
    <link rel="shortcut icon" href="images/loguito.png" type="image/x-icon">
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
